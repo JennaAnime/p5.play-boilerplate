a62389f68035b028151ccdc9c7921422d756f574
JennaAnime <74215571+JennaAnime@users.noreply.github.com>
2021-01-26 17:48:38 -0600

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,27 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.3,
+            length: 300
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            
+            stroke(48,22,8);
+                strokeWeight(4)
+                line(pointA.x , pointA.y, pointB.x , pointB.y );
+                pop()
+            }
+        }
+        }
